91f26041aa6060183578651ec72f286d01f648a8
Mohamed Gamal <mbdr4999@gmail.com>
2024-04-29 23:09:20 +0300

    improving ai

--- oop.cpp
+++ oop.cpp
@@ -102,7 +102,7 @@ private:
 
     int minimax(int depth, bool isMaximizingPlayer, int alpha, int beta) {
         if (checkWin()) {
-            return isMaximizingPlayer ? -1 : 1;
+            return isMaximizingPlayer ? -10 + depth : 10 - depth;
         }
         if (checkTie()) {
             return 0;
@@ -115,70 +115,76 @@ private:
                     if (board[i][j] == ' ') {
                         board[i][j] = player2;
                         int score = minimax(depth + 1, false, alpha, beta);
-                         board[i][j] = ' ';
-                          bestScore = max(bestScore, score); 
-                          alpha = max(alpha, bestScore);
-             if (beta <= alpha)
-              { break; 
-             } 
-         } 
-     } 
-    } return bestScore; 
- }
-       else { int bestScore = numeric_limits<int>::max(); 
-              for (int i = 0; i < 3; i++) 
-             { for (int j = 0; j < 3; j++)
-           { if (board[i][j] == ' ') 
-          { board[i][j] = player1; 
-        int score = minimax(depth + 1, true, alpha, beta);
-         board[i][j] = ' '; bestScore = min(bestScore, score); 
-         beta = min(beta, bestScore); 
-        if (beta <= alpha) 
-         { break;
-          } 
-     } 
-     } 
-      } 
-     return bestScore;
-      } 
-  }
-    bool checkWin() {
-    // Check rows
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
-            return true;
+                        board[i][j] = ' ';
+                        bestScore = max(bestScore, score);
+                        alpha = max(alpha, bestScore);
+                        if (beta <= alpha)
+                            break;
+                    }
+                }
+            }
+            return bestScore;
+        } else {
+            int bestScore = numeric_limits<int>::max();
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == ' ') {
+                        board[i][j] = player1;
+                        int score = minimax(depth + 1, true, alpha, beta);
+                        board[i][j] = ' ';
+                        bestScore = min(bestScore, score);
+                        beta = min(beta, bestScore);
+                        if (beta <= alpha)
+                            break;
+                    }
+                }
+            }
+            return bestScore;
         }
     }
-    // Check columns
-    for (int j = 0; j < 3; j++) {
-        if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ') {
-            return true;
-        }
-    }
-    // Check diagonals
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
-        return true;
-    }
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
-        return true;
-    }
-    return false;
-}
 
-bool checkTie() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == ' ') {
-                return false;
+    bool checkWin() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+                return true;
             }
         }
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ') {
+                return true;
+            }
+        }
+        // Check diagonals
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+            return true;
+        }
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+            return true;
+        }
+        return false;
     }
-    return true;
-}
 
-void switchPlayer() {
-    currentPlayer = (currentPlayer == player1) ? player2 : player1;
-}
+    bool checkTie() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void switchPlayer() {
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    }
 };
 
-int main() { TicTacToe game; game.play(); return 0; }
+int main() {
+    TicTacToe game;
+    game.play();
+    return 0;
+}
+
